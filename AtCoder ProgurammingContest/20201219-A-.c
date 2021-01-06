33e5a634150261eb9aa918ba2b157e069dbfb149
MiuraMao <m_miura06@outlook.com>
2021-01-06 20:36:50 +0900

    Commit

---
+++ AtCoder ProgurammingContest/20201219-A-.c
@@ -0,0 +1,21 @@
+/*
+ * A - Brick
+ */
+
+#include <stdio.h>
+
+int N;
+int W;
+
+int main(void){
+	scanf("%d", &N);
+	scanf("%d", &W);
+
+	if(N>=1){
+		if(W<=1000){
+  			printf("%d\n",N/W);
+  	  }
+  }
+  return 0;
+}
+
