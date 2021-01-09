55e5412fd44e3927d0a73a4bcec02d10a5e0c7d3
MiuraMao <m_miura06@outlook.com>
2021-01-09 23:38:32 +0900

    Commit

---
+++ 20210109-A-.c
@@ -0,0 +1,42 @@
+/*
+ * A - Simple Math 2
+ */
+//不正解
+
+#include <stdio.h>
+
+unsigned long N;
+unsigned long M;
+unsigned long result;
+unsigned long value;
+
+long  factorial(unsigned long N ,unsigned long value,unsigned long i){
+	value *= 10;
+
+	if (i < N) {
+	i++;
+	factorial(N ,value, i) ;
+    }else{
+		return 0;
+    }
+}
+
+
+long main3(void){
+	scanf("%lu", &N);
+	scanf("%lu", &M);
+
+	long int i;
+
+
+  	i = 0;
+	value = 1;
+
+	factorial(N ,value, i);
+
+	result = value / M;
+	printf("%lu\n", result % M);
+
+  return 0;
+}
+
