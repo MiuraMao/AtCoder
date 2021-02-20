d08c9c844409d285efd22ea15b32ecd739d11620
MiuraMao <m_miura06@outlook.com>
2021-02-20 22:51:40 +0900

    Commit

---
+++ 20210220-A-.c
@@ -0,0 +1,23 @@
+/*
+ *A - Star
+ */
+
+#include <stdio.h>
+
+
+int main13(void)
+{
+    int X, result;
+    scanf("%d", &X);
+
+    if (X % 100 >= 1){
+        result = 100 - (X % 100);
+    }else{
+        result = 100;
+    }
+
+    printf("%d\n", result);
+
+    return 0;
+}
+
---
+++ 20210220-B-.c
@@ -0,0 +1,40 @@
+/*
+ *B - uNrEaDaBlE sTrInG
+ */
+
+//不正解・実行時間超過
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void)
+{
+    int i;
+    char ch;
+    int count;
+
+    int result1, result2;
+
+char S[999];
+scanf("%s", S);
+
+    result1 = 0;
+    result2 = 0;
+    for (i = 1; S[i] != 0; i * 2){
+            if (isupper(S[i]) == 0){
+                printf("No");
+                exit(0);
+            }
+    }
+    for (i = 1; i <= count; i * 2 + 1){
+            if (islower(S[i] == 0))
+                printf("No");
+                exit(0);
+    }
+
+    if (result1 == 0 && result2 == 0){
+            printf("Yes");
+    }
+    return 0;
+}
+
