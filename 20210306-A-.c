3258822acf0e01f9e78b5f2e0a5f54c6a593ce09
MiuraMao <m_miura06@outlook.com>
2021-03-06 23:10:45 +0900

    Commit

---
+++ 20210306-A-.c
@@ -0,0 +1,24 @@
+/*
+ * A - I Scream
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B;
+
+    scanf("%d%d", &A, &B);
+
+    if (A+B >= 15 && B >= 8){
+    	printf("1");		//アイスクリーム
+    }else if(A+B >=10 && B >= 3){
+    	printf("2");		//アイスミルク
+    }else if(A+B >= 3){
+    	printf("3");		//ラクトアイス
+    }else{
+    	printf("4");		//氷菓
+    }
+
+}
